f2dd61183b21b0e479edc1c14bbb41bfb99836b7
Ivanyutin Evgeney <e.ivanutin@gmail.com>
2026-04-12 14:58:07 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.88.21', 
-'yatask-ieg.sytes.net', '158.160.212.182']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.88.21',
+                 'yatask-ieg.sytes.net', '158.160.212.182']
 
 
 # Application definition
@@ -60,18 +60,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
-#DATABASES = {
+# }
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': '/data/db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': {
